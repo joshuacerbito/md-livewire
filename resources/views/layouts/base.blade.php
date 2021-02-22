3fb1f3ba4c50fd4c57c16341744315ca45b217e0
Joshua Cerbito <joshuacerbito@gmail.com>
2021-02-22 12:59:05 +0800

    🎨 style preview, add favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
     <title>Document</title>
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
--- resources/views/livewire/markdown-editor.blade.php
+++ resources/views/livewire/markdown-editor.blade.php
@@ -1,5 +1,5 @@
 <section class="editor">
-    <textarea wire:model.debounce.300ms="markdown" id="editor__markdown" class="editor__markdown" autofocus></textarea>
-    <div class="editor__preview">{!! $markdown_preview !!}</div>
+    <textarea wire:model.debounce.300ms="markdown" id="editor__markdown" class="editor__markdown" placeholder="Mark something down 😉" autofocus></textarea>
+    <div class="editor__preview markdown-body">{!! $markdown_preview !!}</div>
     <button class="download-md">Download MD File</button>
 </section>
